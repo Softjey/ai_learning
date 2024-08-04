b70fba3c1d987febe3b46c0b2ee7d2f53b2cd8b6
Softjey <misivsvatoslav@gmail.com>
2024-08-04 18:55:37 +0200

    update real estate prediction to use polynomial of 3 degree and RidgeCV regression

--- tasks/real_estate_prediction/real_estate_prediction.py
+++ tasks/real_estate_prediction/real_estate_prediction.py
@@ -1,13 +1,16 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, KFold, cross_val_score
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
-from sklearn.metrics import mean_absolute_percentage_error
 import os
-import numpy as np
+import time
 
-RANDOM_SEED=413
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import RidgeCV
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import cross_validate
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+
+start = time.time()
+
+RANDOM_SEED = 4135
 
 dataset_path = os.path.join(os.path.dirname(__file__), 'real_estate_dataset.csv')
 estate_dataset = pd.read_csv(dataset_path)
@@ -15,38 +18,31 @@ estate_dataset = pd.read_csv(dataset_path)
 features = estate_dataset.drop(['house_price_of_unit_area', 'no'], axis=1)
 response = np.log(estate_dataset['house_price_of_unit_area'])
 
-poly = PolynomialFeatures(degree=2)
+print('Before poly: ', features.shape)
+
+poly = PolynomialFeatures(degree=3)
 features_poly = poly.fit_transform(features)
 
-train_x, test_x, train_y, test_y = train_test_split(features_poly, response, random_state=RANDOM_SEED)
+print('After poly: ', features_poly.shape)
 
-model = LinearRegression()
-model.fit(train_x, train_y)
-pred_y = model.predict(test_x)
-abs_error = round(mean_absolute_percentage_error(test_y, pred_y) * 100, 2)
+scaler = StandardScaler()
+features_poly = scaler.fit_transform(features_poly)
+
+model = RidgeCV(alphas=np.linspace(0.1, 10, 100))
+
 
 def score_fn(model, x, y):
-  return mean_absolute_percentage_error(y, model.predict(x))
+    return mean_absolute_percentage_error(y, model.predict(x))
 
-kf = KFold(n_splits=5, shuffle=True, random_state=RANDOM_SEED)
-r2_score = cross_val_score(model, features_poly, response, cv=kf, scoring="r2")
-abs_error = cross_val_score(model, features_poly, response, cv=kf, scoring=score_fn)
 
-print('Mean Cross-Validation R^2: ', np.mean(r2_score))
-print(f'Mean Cross-Validation absolute percentage error: {round(np.mean(abs_error) * 100, 2)}%')
+r2_validation = cross_validate(model, features_poly, response, cv=5, scoring="r2", return_estimator=True)
+percentage_error_validation = cross_validate(model, features_poly, response, cv=5, scoring=score_fn, return_train_score=True)
+mean_train_error = np.mean(percentage_error_validation['train_score'])
+mean_test_error = np.mean(percentage_error_validation['test_score'])
+generalization_error = (mean_test_error - mean_train_error) / mean_train_error
 
-# x_generated = np.array(features)
-# x_generated_poly = poly.transform(x_generated)
-# y_pred = model.predict(x_generated_poly)
-
-# for i, column in enumerate(features.columns):
-#   fig, ax = plt.subplots()
-#   column_data = x_generated[:, i]
-#   x_generated_column = np.linspace(min(column_data), max(column_data), 100)
- 
-#   ax.scatter(estate_dataset[column], response, alpha=0.5)
-#   ax.plot(x_generated_column, y_pred, color="orange")
-#   ax.set_xlabel(column)
-#   ax.set_ylabel('House Price of Unit Area')
-  
-#   plt.show()
+print(f'Mean R^2: {np.mean(r2_validation['test_score'])}')
+print(f'Mean Training set MAPE: {round(mean_train_error * 100, 2)}%')
+print(f'Mean Test set MAPE: {round(mean_test_error * 100, 2)}%')
+print(f'Generalization Error: {round(generalization_error * 100, 2)}%')
+print(f'Execution time: {time.time() - start}')
